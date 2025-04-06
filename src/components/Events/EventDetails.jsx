c7a982f7f889fc3ff029944001934e4fb84f3eac
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-04-07 02:31:26 +0300

    modify EventDetails page

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -10,41 +10,43 @@ export default function EventDetails() {
   const id = params.id
 
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ['event', id],
+    queryKey: ['events', id],
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
   });
 
   const { mutate } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['events'] });
+      queryClient.invalidateQueries({ 
+        queryKey: ['events'],
+        refetchType: 'none',
+      });
       navigate('/events');
     }
   });
-
 
   function handleDelete() {
     mutate({ id });
   }
 
+  let content;
+
   if (isPending) {
-    return <p className="text-white text-center mt-8">Loading...</p>;
-  }
+    content = (<div id='event-details-content' className='center'>
+      <p>Fething event...</p>
+    </div>
+    )
+  };
 
   if (isError) {
-    return <p className="text-red-500 text-center mt-8">Error: {error.message}</p>;
+    content = (
+      <ErrorBlock title="Failed to load event." message={error.info?.message || "Failed to fetch event data. Please try again later!"} />
+    )
   }
 
-
-  return (
-    <>
-      <Outlet />
-      <Header>
-        <Link to="/events" className="nav-item">
-          View all Events
-        </Link>
-      </Header>
-      <article id="event-details">
+  if (data) {
+    content = (
+      <>
         <header>
           <h1>{data.title}</h1>
           <nav>
@@ -62,6 +64,20 @@ export default function EventDetails() {
             <p id="event-details-description">{data.description}</p>
           </div>
         </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Outlet />
+      <Header>
+        <Link to="/events" className="nav-item">
+          View all Events
+        </Link>
+      </Header>
+      <article id="event-details">
+        {content}
       </article>
     </>
   );
