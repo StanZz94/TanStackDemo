7e2cb0c8e7571241f0cb4f3c1019c6b37c9b01f0
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-04-07 00:50:11 +0300

    fetch and render event details data

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,19 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 import Header from '../Header.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../util/http.js';
 
 export default function EventDetails() {
+  const params = useParams();
+  const id = params.id
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['event'],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+  console.log(data)
+
   return (
     <>
       <Outlet />
@@ -13,20 +24,20 @@ export default function EventDetails() {
       </Header>
       <article id="event-details">
         <header>
-          <h1>EVENT TITLE</h1>
+          <h1>{data.title}</h1>
           <nav>
             <button>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
         <div id="event-details-content">
-          <img src="" alt="" />
+          <img src={`http://localhost:3000/${data.image}`} alt="image" />
           <div id="event-details-info">
             <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>{data.date + ' at ' + data.time}</time>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
+            <p id="event-details-description">{data.description}</p>
           </div>
         </div>
       </article>
